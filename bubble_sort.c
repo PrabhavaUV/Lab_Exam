f507d0c44baf734e16639166d95b07b56589fe9c
=Prabha <you@example.com>
2026-03-02 10:00:09 +0530

    Sort Algorithm

---
+++ bubble_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    // Traverse through all array elements
+    for (int i = 0; i < n - 1; i++) {
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++) {
+            // Traverse the array from 0 to n-i-1
+            // Swap if the element found is greater than the next element
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("Original array: \n");
+    printArray(arr, n);
+    
+    bubbleSort(arr, n);
+    
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    
+    return 0;
+}
